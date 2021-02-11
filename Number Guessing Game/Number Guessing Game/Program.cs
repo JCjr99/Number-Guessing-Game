0972f31190d6b862eb6ee0da082b4ec2efb1b833
Jacob Cooper <31042350+JCjr99@users.noreply.github.com>
2021-02-11 13:08:38 +0000

    Add files via upload

---
+++ Number Guessing Game/Number Guessing Game/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Number_Guessing_Game
+{
+    class Guessing_Game
+    {
+        private int number;
+        private int noGuesses;
+        public void Play()
+        {
+            noGuesses = 0;
+            Random rdm = new Random();
+            number = rdm.Next(1, 100);
+
+            Console.WriteLine("Guess a number between 1 and 100");
+            int guess =  Convert.ToInt32(Console.ReadLine());
+            while (true)
+            {
+                if (guess < number)
+                {
+                    noGuesses++;
+                    Console.WriteLine("Guess too low! Try again");
+                    Console.WriteLine("Number of guesses:" + noGuesses);
+                    
+                }
+                else if(guess > number)
+                {
+                    noGuesses++;
+                    Console.WriteLine("Guess too high! Try again");                 
+                    Console.WriteLine("Number of guesses:" + noGuesses);
+                    
+                }
+                else if (guess == number)
+                {
+                    noGuesses++;
+                    Console.WriteLine("Correct! The number was " + number);
+                    Console.WriteLine("Total number of guesses: " + noGuesses);
+                    break;
+                }
+                guess = Convert.ToInt32(Console.ReadLine());
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            Guessing_Game game = new Guessing_Game();
+            game.Play();
+        }
+    }
+}
